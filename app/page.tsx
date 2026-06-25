bb25a52c5d9b99a0ce52aac567ac92fdef4e0a9d
smilk007-tech <smilk007@gmail.com>
2026-06-25 22:47:02 +0900

    commit28

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import ToiletGame from "./ToiletGame";
-import InstallPrompt from "@/components/InstallPrompt";
 
 export default function Home() {
-  return (
-    <>
-      <ToiletGame />
-      <InstallPrompt />
-    </>
-  );
+  return <ToiletGame />;
 }
